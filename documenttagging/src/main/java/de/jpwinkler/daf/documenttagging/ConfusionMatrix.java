de0e0a525d3367ef75522efa78ac0646b81166c3
jwinkler <jonas.winkler@tu-berlin.de>
2015-08-03 19:09:20 +0200

    added micro/macro precision/recall/f1 to confusion matrix

--- documenttagging/src/main/java/de/jpwinkler/daf/documenttagging/ConfusionMatrix.java
+++ documenttagging/src/main/java/de/jpwinkler/daf/documenttagging/ConfusionMatrix.java
@@ -5,7 +5,23 @@ import java.util.List;
 
 public class ConfusionMatrix<TAG> {
 
+
+    /**
+     * This matrix is organized as following:
+     *
+     * <pre>
+     *                predicted class
+     *                a  b  c
+     *              a .  .  .
+     *              b .  .  .
+     * actual class c .  .  .
+     * </pre>
+     *
+     * The first dimension (x axis) is used to index predicted classes, the
+     * second dimension (y axis) is used to index actual classes.
+     */
     private final int[][] matrix;
+
     private final List<TAG> tags;
 
     private int sumRow(final int row) {
@@ -37,11 +53,19 @@ public class ConfusionMatrix<TAG> {
                 System.out.println("oops...");
             }
         }
+    }
 
+    public ConfusionMatrix(final List<TAG> tags) {
+        this.tags = tags;
+        matrix = new int[tags.size()][tags.size()];
     }
 
     public int get(final TAG predicted, final TAG actual) {
         return matrix[tags.indexOf(predicted)][tags.indexOf(actual)];
+    }
+
+    public void set(final TAG predicted, final TAG actual, final int count) {
+        matrix[tags.indexOf(predicted)][tags.indexOf(actual)] = count;
     }
 
     public float getPrecision(final TAG tag) {
@@ -66,6 +90,54 @@ public class ConfusionMatrix<TAG> {
 
     public float getF1Score(final TAG tag) {
         return 2.0f * (getPrecision(tag) * getRecall(tag)) / (getPrecision(tag) + getRecall(tag));
+    }
+
+    public float getMicroPrecision() {
+        float a = 0;
+        float b = 0;
+
+        for (int tagIndex = 0; tagIndex < tags.size(); tagIndex++) {
+            a += matrix[tagIndex][tagIndex];
+            b += sumColumn(tagIndex);
+        }
+
+        return a / b;
+    }
+
+    public float getMacroPrecision() {
+        float sum = 0;
+        for (final TAG tag : tags) {
+            sum += getPrecision(tag);
+        }
+        return sum / tags.size();
+    }
+
+    public float getMicroRecall() {
+        float a = 0;
+        float b = 0;
+
+        for (int tagIndex = 0; tagIndex < tags.size(); tagIndex++) {
+            a += matrix[tagIndex][tagIndex];
+            b += sumRow(tagIndex);
+        }
+
+        return a / b;
+    }
+
+    public float getMacroRecall() {
+        float sum = 0;
+        for (final TAG tag : tags) {
+            sum += getRecall(tag);
+        }
+        return sum / tags.size();
+    }
+
+    public float getMicroF1Score() {
+        return 2.0f * (getMicroPrecision() * getMicroRecall()) / (getMicroPrecision() + getMicroRecall());
+    }
+
+    public float getMacroF1Score() {
+        return 2.0f * (getMacroPrecision() * getMacroRecall()) / (getMacroPrecision() + getMacroRecall());
     }
 
     @Override
