61cddd020d8707a8face867a26ed010803a5d5ec
jwinkler <jonas.winkler@tu-berlin.de>
2015-08-04 18:16:17 +0200

    renamed confusion matrix test

--- documenttagging/src/test/java/de/jpwinkler/daf/documenttagging/ConfusionMatrixTest.java
+++ documenttagging/src/test/java/de/jpwinkler/daf/documenttagging/ConfusionMatrixTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestConfusionMatrix {
+public class ConfusionMatrixTest {
 
     private ConfusionMatrix<String> matrix;
 
