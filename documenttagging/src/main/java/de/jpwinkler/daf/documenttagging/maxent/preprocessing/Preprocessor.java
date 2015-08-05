1f0fd316d2867e8abb0dd8d7a6248d6461b9b675
jwinkler <jonas.winkler@tu-berlin.de>
2015-08-05 13:28:56 +0200

    removed unused code

--- documenttagging/src/main/java/de/jpwinkler/daf/documenttagging/maxent/preprocessing/Preprocessor.java
+++
@@ -1,9 +0,0 @@
-package de.jpwinkler.daf.documenttagging.maxent.preprocessing;
-
-import de.jpwinkler.daf.dafcore.model.csv.DoorsObject;
-
-public interface Preprocessor {
-
-    void apply(DoorsObject object);
-
-}
