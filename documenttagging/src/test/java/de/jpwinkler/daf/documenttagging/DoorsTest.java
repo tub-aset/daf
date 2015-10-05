4ac9899b911d23429b94da9216a55f05a4a462ee
jwinkler <jonas.winkler@tu-berlin.de>
2015-10-05 16:02:40 +0200

    doors test

--- documenttagging/src/test/java/de/jpwinkler/daf/documenttagging/DoorsTest.java
+++ documenttagging/src/test/java/de/jpwinkler/daf/documenttagging/DoorsTest.java
@@ -2,16 +2,22 @@ package de.jpwinkler.daf.documenttagging;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import de.jpwinkler.daf.dafcore.csv.ModuleCSVParser;
+import de.jpwinkler.daf.dafcore.csv.SimpleModuleWriter;
 import de.jpwinkler.daf.dafcore.model.csv.DoorsModule;
+import de.jpwinkler.daf.dafcore.model.csv.DoorsObject;
 import de.jpwinkler.daf.dafcore.model.csv.DoorsTreeNode;
 import de.jpwinkler.daf.dafcore.rulebasedmodelconstructor.util.CSVParseException;
 import de.jpwinkler.daf.documenttagging.doors.DoorsDocumentAccessor;
@@ -19,11 +25,15 @@ import de.jpwinkler.daf.documenttagging.doors.maxent.DoorsMaxEntPredicateGenerat
 import de.jpwinkler.daf.documenttagging.doors.preprocessing.DoorsModulePreprocessor;
 import de.jpwinkler.daf.documenttagging.hypermarkovchain.GrowRateFunction;
 import de.jpwinkler.daf.documenttagging.hypermarkovchain.HyperMarkovChainBuilder;
-import de.jpwinkler.daf.documenttagging.hypermarkovchain.smoothing.KatzBackoff;
+import de.jpwinkler.daf.documenttagging.hypermarkovchain.smoothing.AbsoluteDiscountingSmoothing;
 import de.jpwinkler.daf.documenttagging.hypermarkovchain.smoothing.NoSmoothing;
 import de.jpwinkler.daf.documenttagging.maxent.MaxEntPredicateGenerator;
 import de.jpwinkler.daf.documenttagging.maxent.MaxEntRecursiveViterbiAlgorithm;
+import de.jpwinkler.daf.documenttagging.maxent.SimpleMaxEntAlgorithm;
+import de.jpwinkler.daf.documenttagging.maxent.TrainingDataEventStream;
+import de.jpwinkler.daf.documenttagging.maxent.util.MaxentUtils;
 import opennlp.maxent.GIS;
+import opennlp.maxent.GISModel;
 
 public class DoorsTest {
 
@@ -72,44 +82,101 @@ public class DoorsTest {
         GIS.PRINT_MESSAGES = false;
     }
 
-    @Test
+    // @Test
     public void test4() throws Exception {
         final MaxEntPredicateGenerator<DoorsTreeNode> generator = DoorsMaxEntPredicateGenerator.getDefaultGenerator("pod_tag");
         final List<DocumentAccessor<DoorsTreeNode>> documentAccessors = prodModules.values().stream().map(m -> new DoorsDocumentAccessor(m)).collect(Collectors.toList());
-        final DocumentAccessor<DoorsTreeNode> documentToTag = documentAccessors.get(1);
+        final DoorsDocumentAccessor documentToTag = (DoorsDocumentAccessor) documentAccessors.remove(8);
 
-        final DocumentTaggingAlgorithm<DoorsTreeNode, String> alg1 = new MaxEntRecursiveViterbiAlgorithm<DoorsTreeNode>(generator, documentAccessors, new KatzBackoff<>(0, 0.7), GrowRateFunction.LINEAR, 100, 0);
+        final DocumentTaggingAlgorithm<DoorsTreeNode, String> alg1 = new MaxEntRecursiveViterbiAlgorithm<DoorsTreeNode>(generator, documentAccessors, new AbsoluteDiscountingSmoothing<>(0.75), GrowRateFunction.CONSTANT_5, 1000, 0);
         final TaggedDocument<DoorsTreeNode, String> algResult1 = alg1.tagDocument(documentToTag);
         final ConfusionMatrix<String> confusionMatrix1 = new ConfusionMatrix<>(algResult1);
 
-        // final DocumentTaggingAlgorithm<DoorsTreeNode, String> alg2 = new
-        // SimpleMaxEntAlgorithm<DoorsTreeNode>(generator, documentAccessors,
-        // 1000, 0);
-        // final TaggedDocument<DoorsTreeNode, String> algResult2 =
-        // alg2.tagDocument(documentToTag);
-        // final ConfusionMatrix<String> confusionMatrix2 = new
-        // ConfusionMatrix<>(algResult2);
+        final DocumentTaggingAlgorithm<DoorsTreeNode, String> alg2 = new SimpleMaxEntAlgorithm<DoorsTreeNode>(generator, documentAccessors, 1000, 0);
+        final TaggedDocument<DoorsTreeNode, String> algResult2 = alg2.tagDocument(documentToTag);
+        final ConfusionMatrix<String> confusionMatrix2 = new ConfusionMatrix<>(algResult2);
+
+        final SimpleModuleWriter writer = new SimpleModuleWriter(System.out);
+
+        writer.setObjectStringFunction(o -> Arrays.toString(generator.getContextualPredicates(o, false)));
+        writer.setObjectAnnotationFunction(new Function<DoorsObject, String>() {
+            @Override
+            public String apply(final DoorsObject o) {
+                if (algResult1.getActualTag(o).equals(algResult1.getPredictedTag(o))) {
+                    return "||| ok: " + algResult1.getPredictedTag(o);
+                } else {
+                    return "||| actual: " + algResult1.getActualTag(o) + ", rv: " + algResult1.getPredictedTag(o) + ", me: " + algResult2.getPredictedTag(o);
+                }
+            }
+        });
+
+        writer.writeModule(documentToTag.getDoorsModule());
 
         System.out.println(confusionMatrix1.toStringEvaluationMetrics());
-        // System.out.println(confusionMatrix2.toStringEvaluationMetrics());
+        System.out.println(confusionMatrix2.toStringEvaluationMetrics());
     }
 
     // @Test
     public void test3() throws Exception {
+        final MaxEntPredicateGenerator<DoorsTreeNode> generator = DoorsMaxEntPredicateGenerator.getDefaultGenerator("pod_tag");
+        final List<DocumentAccessor<DoorsTreeNode>> documentAccessors = prodModules.values().stream().map(m -> new DoorsDocumentAccessor(m)).collect(Collectors.toList());
+        final DoorsDocumentAccessor documentToTag = (DoorsDocumentAccessor) documentAccessors.remove(2);
 
+        final List<DoorsTreeNode> trainingElements = new ArrayList<>();
+        for (final DocumentAccessor<DoorsTreeNode> documentAccessor : documentAccessors) {
+            documentAccessor.visit(documentAccessor.getDocumentRoot(), e -> trainingElements.add(e));
+        }
 
+        System.out.println("PERSISTENT_CONDITION training objects:");
+        for (final DoorsTreeNode n : trainingElements) {
+            if ("PERSISTENT_CONDITION".equals(generator.getOutcome(n))) {
+                System.out.println(Arrays.toString(generator.getContextualPredicates(n, true)));
+            }
+        }
+
+        System.out.println();
+        System.out.println("TRIGGER training objects:");
+        for (final DoorsTreeNode n : trainingElements) {
+            if ("TRIGGER".equals(generator.getOutcome(n))) {
+                System.out.println(Arrays.toString(generator.getContextualPredicates(n, true)));
+            }
+        }
+
+        final GISModel model = GIS.trainModel(new TrainingDataEventStream<>(generator, trainingElements), 200, 0);
+
+        final DoorsObject object = (DoorsObject) documentToTag.getDoorsModule().getChildren().get(0).getChildren().get(0).getChildren().get(0).getChildren().get(0);
+
+        final String[] contextualPredicates = generator.getContextualPredicates(object, false);
+
+        final double[] eval = model.eval(contextualPredicates);
+
+        final double p1 = eval[model.getIndex("PERSISTENT_CONDITION")];
+        final double p2 = eval[model.getIndex("TRIGGER")];
+
+        System.out.println("predicates for TRIGGER");
+        System.out.println(MaxentUtils.printMostInfluencingPredicates(model, "TRIGGER"));
+        System.out.println("predicates for PERSISTENT_CONDITION");
+        System.out.println(MaxentUtils.printMostInfluencingPredicates(model, "PERSISTENT_CONDITION"));
+
+        System.out.println(MaxentUtils.printMostLikelyOutcomes((String[]) model.getDataStructures()[2], eval, eval.length));
+
+        System.out.println("predicates: " + Arrays.toString(contextualPredicates));
+
+        System.out.println("persistent condition: " + p1);
+        System.out.println("trigger: " + p2);
     }
 
-    // @Test
+    @Test
     public void testMarkovChainBuilder() {
         final MaxEntPredicateGenerator<DoorsTreeNode> generator = DoorsMaxEntPredicateGenerator.getDefaultGenerator("pod_tag");
         final HyperMarkovChainBuilder<String> hyperMarkovChainBuilder = new HyperMarkovChainBuilder<>(new NoSmoothing<>(), GrowRateFunction.LINEAR);
 
-        for (final DoorsModule module : prodModules.values()) {
-            hyperMarkovChainBuilder.addAll(new DoorsDocumentAccessor(module), o -> generator.getOutcome(o), t -> !t.isEmpty());
+        for (final Entry<String, DoorsModule> e : prodModules.entrySet()) {
+            hyperMarkovChainBuilder.addAll(new DoorsDocumentAccessor(e.getValue()), o -> generator.getOutcome(o), t -> !t.isEmpty());
         }
 
         System.out.println(hyperMarkovChainBuilder.build().getDOT(0.0));
+
     }
 
     private void printEvaluationMetrics(final ConfusionMatrix<String> simpleMaxEntConfusionMatrix, final ConfusionMatrix<String> maxEntRecursiveViterbiConfusionMatrix) {
