661057338830d8217557e7ed0d8da45f59c048b7
jwinkler <jonas.winkler@tu-berlin.de>
2016-01-21 18:37:37 +0100

    added missing code required for the preprocess integration in csveditor

--- dataprocessing/src/main/java/de/jpwinkler/daf/dataprocessing/preprocessing/ObjectTextPreprocessor.java
+++ dataprocessing/src/main/java/de/jpwinkler/daf/dataprocessing/preprocessing/ObjectTextPreprocessor.java
@@ -1,7 +1,10 @@
 package de.jpwinkler.daf.dataprocessing.preprocessing;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 
 import de.jpwinkler.libs.stringprocessing.patternprogram.PatternProgram;
@@ -10,21 +13,57 @@ import de.jpwinkler.libs.stringprocessing.tokens.Tokenizer;
 
 public class ObjectTextPreprocessor {
 
-    private final PatternProgram program;
+    private PatternProgram program;
+    private boolean enabled;
 
     public ObjectTextPreprocessor(final PatternProgram program) {
         super();
         this.program = program;
+        enabled = true;
     }
 
+
     public List<Token> preprocessText(final String text) {
-        final List<Token> tokens = Tokenizer.tokenizeString(text);
-        return program.execute(tokens);
+        if (enabled) {
+            final List<Token> tokens = Tokenizer.tokenizeString(text);
+            return program.execute(tokens);
+        } else {
+            return Tokenizer.tokenizeString(text);
+        }
     }
 
     public String preprocessTextToString(final String text) {
-        final List<Token> tokens = Tokenizer.tokenizeString(text);
-        return StringUtils.join(program.execute(tokens), " ");
+        if (enabled) {
+            return StringUtils.join(preprocessText(text), " ");
+        } else {
+            return text;
+        }
     }
 
+    public static ObjectTextPreprocessor getDefaultPreprocessor() {
+        try {
+            final InputStream stream = ObjectTextPreprocessor.class.getClassLoader().getResourceAsStream("preprocess.pp");
+            return new ObjectTextPreprocessor(PatternProgram.compile(IOUtils.toString(stream)));
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static ObjectTextPreprocessor getEmptyDisabledPreprocessor() {
+        final ObjectTextPreprocessor objectTextPreprocessor = new ObjectTextPreprocessor(PatternProgram.compile(""));
+        objectTextPreprocessor.setEnabled(false);
+        return objectTextPreprocessor;
+    }
+
+    public PatternProgram getProgram() {
+        return program;
+    }
+
+    public void setProgram(final PatternProgram program) {
+        this.program = program;
+    }
+
+    public void setEnabled(final boolean enabled) {
+        this.enabled = enabled;
+    }
 }
