31516c475de2b60bc8e367d31d4a020ea1cc62fd
jwinkler <jonas.winkler@tu-berlin.de>
2016-03-22 18:37:34 +0100

    added date parsing utility method

--- dafcore/src/main/java/de/jpwinkler/daf/dafcore/util/DoorsModuleUtil.java
+++ dafcore/src/main/java/de/jpwinkler/daf/dafcore/util/DoorsModuleUtil.java
@@ -1,5 +1,10 @@
 package de.jpwinkler.daf.dafcore.util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import de.jpwinkler.daf.dafcore.model.csv.DoorsObject;
 import de.jpwinkler.daf.dafcore.model.csv.DoorsTreeNode;
 
@@ -27,4 +32,7 @@ public class DoorsModuleUtil {
 
     }
 
+    public static Date parseDate(final String doorsDateString) throws ParseException {
+        return new SimpleDateFormat("d MMMM yyyy", Locale.ENGLISH).parse(doorsDateString);
+    }
 }
