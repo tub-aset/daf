68716d0462f266f221a7771c83b5f5230d9a0a20
Florian Wiesweg <f.wiesweg@tu-berlin.de>
2019-04-29 14:50:25 +0200

    Make csv editor run on java 11

--- csveditor/src/main/java/de/jpwinkler/daf/csveditor/CSVEditorApplication.java
+++ csveditor/src/main/java/de/jpwinkler/daf/csveditor/CSVEditorApplication.java
@@ -22,47 +22,39 @@ public class CSVEditorApplication extends Application {
 
     private Stage primaryStage;
 
-    public CSVEditorApplication() {
-        super();
-    }
-
     @Override
     public void start(final Stage primaryStage) throws Exception {
         this.primaryStage = primaryStage;
-        try {
-            final FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/csveditor.fxml"));
-            final Parent root = loader.load();
+        final FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/csveditor.fxml"));
+        final Parent root = loader.load();
 
-            csvEditorController = loader.getController();
-            csvEditorController.setStage(primaryStage);
+        csvEditorController = loader.getController();
+        csvEditorController.setStage(primaryStage);
 
-            final Scene scene = new Scene(root);
-            scene.setOnDragOver(event -> {
-                if (event.getDragboard().hasFiles()) {
-                    event.acceptTransferModes(TransferMode.COPY);
-                } else {
-                    event.consume();
-                }
-            });
-            scene.setOnDragDropped(event -> {
-                if (event.getDragboard().hasFiles()) {
-                    for (final File file : event.getDragboard().getFiles()) {
-                        try {
-                            csvEditorController.newTabFromFile(file);
-                        } catch (final Exception e) {
-                            LOGGER.log(Level.SEVERE, e.getMessage(), e);
-                        }
+        final Scene scene = new Scene(root);
+        scene.setOnDragOver(event -> {
+            if (event.getDragboard().hasFiles()) {
+                event.acceptTransferModes(TransferMode.COPY);
+            } else {
+                event.consume();
+            }
+        });
+        scene.setOnDragDropped(event -> {
+            if (event.getDragboard().hasFiles()) {
+                for (final File file : event.getDragboard().getFiles()) {
+                    try {
+                        csvEditorController.newTabFromFile(file);
+                    } catch (final Exception e) {
+                        LOGGER.log(Level.SEVERE, e.getMessage(), e);
                     }
-                    event.setDropCompleted(true);
                 }
-            });
-            primaryStage.setScene(scene);
-            primaryStage.setMaximized(true);
-            primaryStage.setTitle("CSV Editor");
-            primaryStage.show();
-        } catch (final IOException e) {
-            LOGGER.log(Level.SEVERE, e.getMessage(), e);
-        }
+                event.setDropCompleted(true);
+            }
+        });
+        primaryStage.setScene(scene);
+        primaryStage.setMaximized(true);
+        primaryStage.setTitle("CSV Editor");
+        primaryStage.show();
     }
 
     public void openFile(final File file) {
@@ -74,10 +66,6 @@ public class CSVEditorApplication extends Application {
             }
         }
 
-    }
-
-    public static void main(final String[] args) {
-        launch(args);
     }
 
     public Stage getPrimaryStage() {
