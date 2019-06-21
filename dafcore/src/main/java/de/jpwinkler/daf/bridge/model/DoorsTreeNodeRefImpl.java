d62f871b9f08ac8df871b383e7751883d879646d
Florian Wiesweg <f.wiesweg@tu-berlin.de>
2019-06-21 12:54:21 +0200

    Fix license headers

--- dafcore/src/main/java/de/jpwinkler/daf/bridge/model/DoorsTreeNodeRefImpl.java
+++ dafcore/src/main/java/de/jpwinkler/daf/bridge/model/DoorsTreeNodeRefImpl.java
@@ -1,21 +1,26 @@
-/*
- * doorsbridge - A library for Java to Doors interaction.
- * Copyright (C) 2016 Jonas Winkler
- *
+package de.jpwinkler.daf.bridge.model;
+
+/*-
+ * #%L
+ * dafcore
+ * %%
+ * Copyright (C) 2019 TU Berlin ASET
+ * %%
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
  */
-package de.jpwinkler.daf.bridge.model;
 
 import de.jpwinkler.daf.bridge.DoorsApplication;
 import de.jpwinkler.daf.bridge.DoorsItemType;
